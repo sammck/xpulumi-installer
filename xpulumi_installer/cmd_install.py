1a2fc4b81cf3e2e0636004e17462daf0805c5a69
Sam McKelvie <dev@mckelvie.org>
2022-04-19 16:41:53 -0700

    script

--- xpulumi_installer/cmd_install.py
+++ xpulumi_installer/cmd_install.py
@@ -89,7 +89,7 @@ class CmdInstall(CommandHandler):
     subprocess.check_call(['pip3', 'install', 'grpcio==1.43'], cwd=project_root_dir, env=self.get_venv_eviron())
     subprocess.check_call(['pip3', 'install', '--upgrade', xpulumi_package], cwd=project_root_dir, env=self.get_venv_eviron())
     return subprocess.call(
-        [os.path.join(os.path.expanduser('~'), '.venv', 'bin', 'xpulumi'), 'init-env', '--phase-two'],
+        [os.path.join(project_root_dir, '.venv', 'bin', 'xpulumi'), 'init-env', '--phase-two'],
         cwd=project_root_dir, env=self.get_venv_eviron()
       )
 
